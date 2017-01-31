e24f526c43665e5621207f8b3af13d022d618ac7
Admin <hoseingohari76@gmail.com>
2017-01-31 07:13:05 +0330

    Add existing file

---
+++ SOSvr/build/explore/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/workspace/sos/SOSvr/src/explore
+# Build directory: /home/user/workspace/sos/SOSvr/build/explore
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(_ctest_explore_lite_roslaunch-check_launch "/home/user/workspace/sos/SOSvr/build/catkin_generated/env_cached.sh" "/usr/bin/python" "/opt/ros/indigo/share/catkin/cmake/test/run_tests.py" "/home/user/workspace/sos/SOSvr/build/test_results/explore_lite/roslaunch-check_launch.xml" "--return-code" "/usr/bin/cmake -E make_directory /home/user/workspace/sos/SOSvr/build/test_results/explore_lite" "/opt/ros/indigo/share/roslaunch/cmake/../scripts/roslaunch-check -o '/home/user/workspace/sos/SOSvr/build/test_results/explore_lite/roslaunch-check_launch.xml' '/home/user/workspace/sos/SOSvr/src/explore/launch' ")
---
+++ SOSvr/build/map_merge/CMakeFiles/combine_grids.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+FILE(REMOVE_RECURSE
+  "/home/user/workspace/sos/SOSvr/devel/lib/libcombine_grids.a"
+)
